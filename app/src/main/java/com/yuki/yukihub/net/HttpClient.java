538496d57eb355b805abd7af25779e765f876420
xm486 <3086678755@qq.com>
2026-06-19 04:26:02 +0000

    fix: remove unused Date import in HttpClient

--- app/src/main/java/com/yuki/yukihub/net/HttpClient.java
+++ app/src/main/java/com/yuki/yukihub/net/HttpClient.java
@@ -2,7 +2,6 @@ package com.yuki.yukihub.net;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
